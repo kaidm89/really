fa60447ca26354e9370143a327f79c9c720d0d02
PROG-404 <appera8900@gmail.com>
2024-12-26 11:47:27 +0300

    Update and rename READMY.lua to READMY.md

--- READMY.lua
+++
@@ -1,6 +0,0 @@
- These are the PROG-404 library codes for programming scripts
-
-### Lyabrary 
-
-
-# local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-404/really/refs/heads/main/PORG-404-LY.lua"))()
---
+++ READMY.md
@@ -0,0 +1,5 @@
+ These are the PROG-404 library codes for programming scripts
+
+Lyabrary 
+
+- local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PROG-404/really/refs/heads/main/PORG-404-LY.lua"))()
